e1a9306263b9cd9fd8521120396ab2fcbd1d96e6
papapadz <benpadz08@gmail.com>
2016-10-03 01:30:10 +0800

    Added parsing and directory filtering

--- FileIndexing.java
+++ FileIndexing.java
@@ -14,48 +14,58 @@ public class FileIndexing {
 	private static JSONObject obj = new JSONObject();
 	private static JSONArray j_array_key = new JSONArray();
 	private static File fn;
+	private static int fc = 0;
+	private static String d; 
+	private static String k;
 	
 	public static void main(String[] args) throws Exception {
 		
 	Scanner r = new Scanner(System.in);
 	
 	int flag = 0;
-	obj.put("keyword", "");
+	obj.put("keyword", ""); //create new JSON objects
+	obj.put("dir", "");
 	
 	do {	
 		x = 0;
 		System.out.print("Enter keyword to find: ");
-		/*no need lower case*/
-		String keyword = r.next(); //.toLowerCase(); //converts the entered keyword to lower case.
+		
+		k = r.next().toLowerCase(); //all input is set to lower case.
 		
 		System.out.print("Enter directory to search: ");
-		String myDir = r.next();
+		String myDir = r.next().toLowerCase();
 		
 		System.out.println("Search Results:");
 		
-		File dir = new File(myDir);
-		long start = System.nanoTime();
+		File dir = new File(myDir); //creates a new File object
+		d = dir.toString();
+		long start = System.nanoTime(); //record start time
 		
-		int i = 1;
-		if(keyword.equalsIgnoreCase(obj.getString("keyword"))) {
+		int i = 1; //counter for JSON Objects 
+		if(k.equals(obj.getString("keyword")) && (d.equals(obj.getString("dir")))) {//System.out.println("case 1");
 			for(i = 1; i<=obj.getInt("count");i++)
 				System.out.println(j_array_key.getString(i));
 				
-			Long exec = getExecTime(start);
+			Long exec = getExecTime(start); // calculate execution time
 			System.out.println(obj.get("count")  + " result\\s found in " + exec + " milliseconds");
 			
 		}
 		else
-		if(readFileIndex(keyword)) {
-			parseIndexFile();
+		if(readFileIndex(0)) { //System.out.println("case 2");
 			for(i = 1; i<j_array_key.length();i++)
 				System.out.println(j_array_key.getString(i));
+			
+			obj.put("dir", d);			//create new JSON Objects
+			obj.put("keyword", k);
+			obj.put("paths", j_array_key);
+			obj.put("count", j_array_key.length()-1);
 			
 			Long exec = getExecTime(start);
 			System.out.println( (i-1) + " result\\s found in " + exec + " milliseconds");
 		}
-		else
-			search(keyword, dir, start);
+		else {//System.out.println("case 3");
+			search(dir, start); //recursive search
+		}
 		
 		System.out.print("Retry? [1] - YES, [0] - NO: ");
 		flag = r.nextInt();
@@ -64,14 +74,14 @@ public class FileIndexing {
 		
 	}
 	
-	private static void search(String keyword, File dir, long start) throws Exception {
+	private static void search(File dir, long start) throws Exception {
 		obj.remove("paths");
-		if(getList(keyword,dir)>0) {
-			//obj.put("directory", dir.toString());
-			obj.put("keyword", keyword);
+		if(getList(dir)>0) { //if found at least 1 file, create JSON Object 
+			obj.put("dir", d);
+			obj.put("keyword", k);
 			obj.put("paths", j_array_key);
 			obj.put("count", x);
-			writeFile(keyword);
+			writeFile(); //create text file for indexes
 		}
 		
 		Long exec = getExecTime(start);
@@ -79,17 +89,19 @@ public class FileIndexing {
 		
 	}
 	
-	private static void parseIndexFile() throws JSONException {
+	private static boolean parseIndexFile() throws JSONException {
 		
-		BufferedReader br = null;
+		BufferedReader br = null; 
 		
 		try {
-				String thisLine;
-				br = new BufferedReader(new FileReader(fn));
-				thisLine = br.readLine();
+				br = new BufferedReader(new FileReader(fn)); //read JSON format array in the index file
+				String thisLine = br.readLine();
 
 					JSONObject jo = new JSONObject(thisLine);
-					j_array_key = jo.getJSONArray("paths");
+					if(jo.get("dir").equals(d)) {	//if the same directory, get all the path 
+						j_array_key = jo.getJSONArray("paths"); //save result to JSON Array
+						return true;
+					}
 					
 	
 		} catch (IOException e) {
@@ -103,36 +115,40 @@ public class FileIndexing {
 			}
 		}
 
+		return false;
+		
 	}
 	
-	private static int getList(String keyword, final File dir) throws JSONException {
+	private static int getList(final File dir) throws JSONException {
 		for (final File filename : dir.listFiles()) {
 	        if (filename.isDirectory()) 
-	            getList(keyword,filename);
+	            getList(filename); //recursive search
 	        else {
-	        	if(readFile(keyword, filename.toString())) {
-	            	x++;
+	        	if(readFile(filename.toString())) {	//read file and search for the keyword
+	            	x++;							//add to counter and add JSON Object to JSON Array
 	            	j_array_key.put(x, filename.toString());
-	            	System.out.println(filename);	
+	            	System.out.println(filename);	//display file
 	            }
 	        }
 	    }
-		return x;
+		return x; //returns the number of files searched
 	}
 	
-	private static boolean readFile(String k, String path) {
+	private static boolean readFile(String path) {
 		BufferedReader br = null;
 		try {
 			String thisLine;
 			br = new BufferedReader(new FileReader(path));
-			while ((thisLine = br.readLine()) != null) {
-				String[] words = thisLine.split(" ");
+			while ((thisLine = br.readLine()) != null) { //reads text file per line
+				String[] words = thisLine.split(" "); //stores every word in an Array 
 				int i = 0; 
 				
 				while(i<words.length) {
-					/*made use of ignore case rather than to lower case*/
-					if(k.equalsIgnoreCase(words[i]))
-						return true;
+					
+					if(words[i].length()>=k.length()) //compare character count 
+						if((words[i]).toLowerCase().contains(k)) // parsing made through CONTAINS function
+							return true;
+					
 					i++;
 				}
 			}
@@ -154,33 +170,45 @@ public class FileIndexing {
 		
 		long endTime = System.nanoTime();
 
-		long duration = (endTime - startTime)/1000000;
+		long duration = (endTime - startTime)/1000000; //result in milliseconds
 		return duration;
 	}
 	
-	private static void writeFile(String k) throws Exception{
-		File file = new File("C:\\index\\" + k + ".txt");
+	private static void writeFile() throws Exception{
+		String n = k;
+		readFileIndex(1); //check the same index file 
+		if(fc>0) { //if duplicate file, change filename
+			readFileIndex(0);	
+			n = n+fc;
+		}
+		
+		File file = new File("C:\\index\\" + n + ".txt"); // create a new file
 		
 		try (FileWriter fw = new FileWriter(file)) {
-			fw.write(obj.toString());
+			fw.write(obj.toString()); // write the JSON format into file
 		}
 		
 	}
 	
-	private static boolean readFileIndex(String keyword) throws Exception, ParseException {
+	private static boolean readFileIndex(int state) throws Exception, ParseException {
 		
-		File indexDir = new File("C:\\index");
+		if(state==1) //1 for counting index files, 0 for reading index files
+			fc = 0;
+		
+		File indexDir = new File("C:\\index"); //default index file save location
 			
 		for (final File filename : indexDir.listFiles()) {
 			String f = filename.getName();
 			
-			if(f.startsWith(keyword)) {
+			if(f.startsWith(k)) { //check filename
 				fn = filename;
-				return true;
+				if(state==0) {
+					if(parseIndexFile()) //read index file
+						return true;
+				} else	 
+					fc++; //counter for the number of index files with the same filename
 			}
 		}
-			
-	 return false;
-		
-	}	
+	 return false;		
+	}
 }
